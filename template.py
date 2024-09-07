14b2e44d07eb27e5bce9a8e1678c59b73637cd70
saheb <sahebs450@gmail.com>
2024-09-07 23:03:34 +0530

    add all require files

--- template.py
+++ template.py
@@ -1,53 +1,57 @@
 import os
 from pathlib import Path
-import logging 
-logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s:')
+import logging
 
-project_name="hate"
-list_of_files=[
-    f"{project_name}/component/__init__.py",
-    f"{project_name}/component/data_ingestion.py",
-    f"{project_name}/component/tranformation.py",
-    f"{project_name}/component/model_evaluation.py",
-    f"{project_name}/component/model_trainer.py",
-    f"{project_name}/component/model_pusher.py",
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
+
+project_name = "hate"
+
+list_of_files = [
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_transforamation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
     f"{project_name}/configuration/__init__.py",
-    f"{project_name}/configuration/gcloud_sysncer.py",
-
+    f"{project_name}/configuration/gcloud_syncer.py",
     f"{project_name}/constants/__init__.py",
-
     f"{project_name}/entity/__init__.py",
     f"{project_name}/entity/config_entity.py",
     f"{project_name}/entity/artifact_entity.py",
-
     f"{project_name}/exception/__init__.py",
     f"{project_name}/logger/__init__.py",
-
     f"{project_name}/pipeline/__init__.py",
-    f"{project_name}/pipeline/prediction.py",
-    f"{project_name}/component/train_pipeline.py",
-
+    f"{project_name}/pipeline/train_pipeline.py",
+    f"{project_name}/pipeline/prediction_pipeline.py",
     f"{project_name}/ml/__init__.py",
-    f"{project_name}/ml/feature.py",
-    f"{project_name}/ml/models.py",
+    f"{project_name}/ml/model.py",
     "app.py",
     "demo.py",
-    "requirments.txt",
+    "requirements.txt",
     "Dockerfile",
     "setup.py",
-    ".dockerignore"   
+    ".dockerignore"
 ]
+
+
+
+
 for filepath in list_of_files:
-    filepath=path(filepath)
+    filepath = Path(filepath)
+
     filedir, filename = os.path.split(filepath)
 
-    if filedir !=" ":
-        os.makedirs(filedir,exist_ok=True)
-        logging.info(f"crating directory: {filedir} for the file: {filename}")
-    if(not os.path.exists(filepath)) or  (os.path.getsize(filepath)==0):
-        with open(filepath ,"w")as f:
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
             pass
-            logging.info(f"Creating empty file:{filepath}")
-    else :
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
         logging.info(f"{filename} is already exists")
