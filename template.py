45ba5013080cb542781e05a805a7a17440315e3a
saheb <sahebs450@gmail.com>
2024-09-07 22:55:44 +0530

    changes at template

--- template.py
+++ template.py
@@ -4,14 +4,14 @@ import logging
 logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s:')
 
 project_name="hate"
-list_of_files={
+list_of_files=[
     f"{project_name}/component/__init__.py",
     f"{project_name}/component/data_ingestion.py",
     f"{project_name}/component/tranformation.py",
     f"{project_name}/component/model_evaluation.py",
     f"{project_name}/component/model_trainer.py",
     f"{project_name}/component/model_pusher.py",
-    
+
     f"{project_name}/configuration/__init__.py",
     f"{project_name}/configuration/gcloud_sysncer.py",
 
@@ -27,9 +27,27 @@ list_of_files={
     f"{project_name}/pipeline/__init__.py",
     f"{project_name}/pipeline/prediction.py",
     f"{project_name}/component/train_pipeline.py",
-    
 
+    f"{project_name}/ml/__init__.py",
+    f"{project_name}/ml/feature.py",
+    f"{project_name}/ml/models.py",
+    "app.py",
+    "demo.py",
+    "requirments.txt",
+    "Dockerfile",
+    "setup.py",
+    ".dockerignore"   
+]
+for filepath in list_of_files:
+    filepath=path(filepath)
+    filedir, filename = os.path.split(filepath)
 
-
-    
-}
+    if filedir !=" ":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"crating directory: {filedir} for the file: {filename}")
+    if(not os.path.exists(filepath)) or  (os.path.getsize(filepath)==0):
+        with open(filepath ,"w")as f:
+            pass
+            logging.info(f"Creating empty file:{filepath}")
+    else :
+        logging.info(f"{filename} is already exists")
